c4415e176a13496b3e63f8029d559b425c2efecb
SivtsovaT <sivtsovatanya1985@gmail.com>
2021-09-06 11:34:11 +0300

    Implement GET /applesByKind to return records by kind (using SQL IN)
    - removed public scheme into query's value into all functions
    - created apple-files into migrations directory and updated migrations

--- apple/apple_repository.js
+++ apple/apple_repository.js
@@ -4,7 +4,7 @@ const createApple = (color, size, region, harvestInTon) => {
 
     return pool.connect()
         .then(client => {
-            const query = `INSERT INTO public.apples (color, size, region, harvest_in_ton)
+            const query = `INSERT INTO apples (color, size, region, harvest_in_ton)
                            VALUES ('${color}', '${size}', '${region}', '${harvestInTon}') RETURNING *`;
             return client.query(query)
         })
@@ -20,7 +20,7 @@ const getApples = (offset, limit) => {
     return pool.connect()
         .then(client => {
             const query = `SELECT apple_id, color, size, region, harvest_in_ton
-                           FROM public.apples ORDER BY color
+                           FROM apples ORDER BY color
                            LIMIT ${limit} OFFSET ${offset}
                            `;
             return client.query(query)
@@ -34,7 +34,7 @@ const deleteAppleById = (appleId) => {
     return pool.connect()
         .then(client => {
             const query = `DELETE
-                           FROM public.apples
+                           FROM apples
                            WHERE apple_id = ${appleId}`;
             return client.query(query)
         })
